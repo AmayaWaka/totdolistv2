f346e544585572cf4ac3513963343abb12c30aad
AmayaWaka <rmadegwa63@gmail.com>
2022-09-08 22:53:29 +0300

    Beginning of todo list app

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+//
+// app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({extended : true}));
+//
 
-const bodyParser = require("body-parser");
+let express = require('express');
+let app = express();
 app.set('view engine', 'ejs');
 
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.get('/', function(req, res){
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-app.get("/", function(req, res){
+  if (currentDay === 6 || currentDay === 0){
+    day = "Weekend";
 
-  res.sendFile(__dirname + "/index.html");
-})
+  }else{
+    day = "Weekday";
+  }
+  res.render("index", { kindOfDay: day })
 
 
-app.listen(3000, function(){
-  console.log("Server Started On Port 3000")
-})
+});
+
+app.listen(3000, function(req, res){
+  console.log("Server Started on Port 3000")
+});
