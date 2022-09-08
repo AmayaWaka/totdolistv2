23e14a62f74fd23c240b9dafcec33a03ea9dc8f6
AmayaWaka <rmadegwa63@gmail.com>
2022-09-08 15:04:46 +0300

    Created The Server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.get("/", function(req, res){
+
+  res.sendFile(__dirname + "/index.html");
+})
+
+
+app.listen(3000, function(){
+  console.log("Server Started On Port 3000")
+})
